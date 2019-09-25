e2331019f0a58bf6c141f7bbbef0176f71dcf2fc
Susan Meyers <smmeyers24@gmail.com>
2019-09-25 00:59:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
  def speak_to_grandma(phrase)
    if phrase == "I LOVE YOU GRANDMA!"
-     return "I LOVE YOU PUMPKIN!"
+     return "I LOVE YOU TOO PUMPKIN!"
    end
    
-   if phrase == phase.upcase
+   if phrase == phrase.upcase
      return "NO, NOT SINCE 1938!"
    
     else 
